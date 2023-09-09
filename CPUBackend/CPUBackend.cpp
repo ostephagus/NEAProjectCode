854edbc1f9044a34a2b9205a27191672dfe5f93f
ostephagus <37499766+ostephagus@users.noreply.github.com>
2023-09-09 10:52:34 +0100

    Finished testing discrete derivatives

--- CPUBackend/CPUBackend.cpp
+++ CPUBackend/CPUBackend.cpp
@@ -46,9 +46,11 @@ void GranularTesting() {
     velocities.y = testv;
     DoubleReal stepsizes;
     stepsizes.x = 0.1;
-    stepsizes.y = 0.2;
+    stepsizes.y = 1.25;
 
-    std::cout << SecondPuPx(velocities.x, 1, 1, stepsizes.x);
+    std::cout << PvSquaredPy(velocities.y, 1, 1, stepsizes.y, 1.7);
+    FreeMatrix(testu, 3);
+    FreeMatrix(testv, 3);
 }
 
 void TestAll()
@@ -135,17 +137,6 @@ void TestAll()
     FreeMatrix(FG.x, iMax);
     FreeMatrix(FG.y, iMax);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
 int main() {
     GranularTesting();
