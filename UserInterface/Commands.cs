145e772c776f4a83d6132e1677330687b22592a4
ostephagus <37499766+ostephagus@users.noreply.github.com>
2023-09-28 10:10:41 +0100

    Added the panel switching functionality of SimulationScreen

--- UserInterface/Commands.cs
+++ UserInterface/Commands.cs
@@ -23,28 +23,9 @@ namespace UserInterface
             }
         }
     }
-
-    public class ShowSidePanel : ICommand
-    {
-        public event EventHandler? CanExecuteChanged;
-
-        public bool CanExecute(object? parameter) { return true; }
-
-        public void Execute(object? parameter)
-        {
-            //Somehow figure out a way to change the shown screen without access to the instance of the SimulationScreen
-        }
-    }
     public struct WindowChangeParameter
     {
         public Type NewWindow { get; set; }
         public bool IsPopup { get; set; }
-    }
-
-    public enum SidePanel //Different side panels on SimluationScreen
-    {
-        Parameters,
-        AdvancedParameters,
-        VisualisationSettings
     }
 }
--- UserInterface/SimulationScreen.xaml.cs
+++ UserInterface/SimulationScreen.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,12 +19,58 @@ namespace UserInterface
     /// <summary>
     /// Interaction logic for SimulationScreen.xaml
     /// </summary>
-    public partial class SimulationScreen : UserControl
+    public partial class SimulationScreen : UserControl, INotifyPropertyChanged
     {
+        private SidePanelButton? currentButton;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string? CurrentButton //Conversion between string and internal enum value done in property
+        {
+            get {
+                if (currentButton == null) return null;
+                return Enum.GetName(typeof(SidePanelButton), currentButton);
+            }
+            set
+            {
+                if (value == null)
+                {
+                    currentButton = null;
+                }
+                else
+                {
+                    currentButton = (SidePanelButton)Enum.Parse(typeof(SidePanelButton), value);
+                }
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(currentButton)));
+            }
+        }
+
         public SimulationScreen()
         {
             InitializeComponent();
             DataContext = this;
+            currentButton = null;
+        }
+
+        private void panelButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = ((FrameworkElement)sender).Name;
+            if (name == CurrentButton) //If the button of the currently open panel is clicked, close all panels (null)
+            {
+                CurrentButton = null;
+            }
+            else
+            {
+                CurrentButton = name; //If any other panel is open, or no panel is open, open the one corresponding to the button.
+            }
+        }
+
+        public enum SidePanelButton //Different side panels on SimluationScreen
+        {
+            btnParametersSelect,
+            btnUnitsSelect,
+            btnVisualisationSettingsSelect,
+            btnRecordingSelect
         }
     }
 }
