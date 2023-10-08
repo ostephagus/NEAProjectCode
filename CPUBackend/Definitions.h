558fc7fa25f2d2263bd7eb47cc9764ff88de8e53
Sam <37499766+ostephagus@users.noreply.github.com>
2023-10-08 16:50:06 +0100

    Added some documentation for definitions

--- CPUBackend/Definitions.h
+++ CPUBackend/Definitions.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// Definitions for boundary cells. For the last 5 bits, format is [self] [north] [east] [south] [west]
+// Where 1 means the corresponding cell is fluid, and 0 means the corresponding cell is obstacle.
+// Boundary cells are defined as obstacle cells with fluid on 1 side or 2 adjacent sides
+// For fluid cells, XOR the corresponding inverse boundary with FLUID.
 #define B_N   0b00001000
 #define B_NE  0b00001100
 #define B_E   0b00000100
