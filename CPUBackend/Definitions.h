15b608a320b100841bfc569003c061dd56a4f614
Sam <37499766+ostephagus@users.noreply.github.com>
2023-10-08 11:30:35 +0100

    Update definitions to include cell type bit patterns

--- CPUBackend/Definitions.h
+++ CPUBackend/Definitions.h
@@ -1,4 +1,16 @@
 #pragma once
+
+#define B_N   0b00001000
+#define B_NE  0b00001100
+#define B_E   0b00000100
+#define B_SE  0b00000110
+#define B_S   0b00000010
+#define B_SW  0b00000011
+#define B_W   0b00000001
+#define B_NW  0b00001001
+#define OBS   0b00000000
+#define FLUID 0b00011111
+
 typedef double REAL;
 
 
