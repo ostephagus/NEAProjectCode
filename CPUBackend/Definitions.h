cc94fa59a2e0972db739211d8ff4e19fe372ece4
Sam <37499766+ostephagus@users.noreply.github.com>
2023-10-08 17:17:06 +0100

    Added byte typedef, converted define to constexpr, added init_FLAG

--- CPUBackend/Definitions.h
+++ CPUBackend/Definitions.h
@@ -1,21 +1,22 @@
 #pragma once
 
+typedef double REAL;
+typedef unsigned __int8 BYTE;
+
 // Definitions for boundary cells. For the last 5 bits, format is [self] [north] [east] [south] [west]
 // Where 1 means the corresponding cell is fluid, and 0 means the corresponding cell is obstacle.
 // Boundary cells are defined as obstacle cells with fluid on 1 side or 2 adjacent sides
 // For fluid cells, XOR the corresponding inverse boundary with FLUID.
-#define B_N   0b00001000
-#define B_NE  0b00001100
-#define B_E   0b00000100
-#define B_SE  0b00000110
-#define B_S   0b00000010
-#define B_SW  0b00000011
-#define B_W   0b00000001
-#define B_NW  0b00001001
-#define OBS   0b00000000
-#define FLUID 0b00011111
-
-typedef double REAL;
+constexpr BYTE B_N   = 0b00001000;
+constexpr BYTE B_NE  = 0b00001100;
+constexpr BYTE B_E   = 0b00000100;
+constexpr BYTE B_SE  = 0b00000110;
+constexpr BYTE B_S   = 0b00000010;
+constexpr BYTE B_SW  = 0b00000011;
+constexpr BYTE B_W   = 0b00000001;
+constexpr BYTE B_NW  = 0b00001001;
+constexpr BYTE OBS   = 0b00000000;
+constexpr BYTE FLUID = 0b00011111;
 
 
 struct DoubleField
--- CPUBackend/Init.cpp
+++ CPUBackend/Init.cpp
@@ -13,7 +13,27 @@ REAL** MatrixMAlloc(int xLength, int yLength) {
 	return matrix;
 }
 
+BYTE** FlagMatrixMAlloc(int xLength, int yLength) {
+
+	// Create array of pointers pointing to more arrays
+	BYTE** matrix = new BYTE* [xLength];
+
+	//Create the arrays inside each outer array
+	for (int i = 0; i < xLength; ++i) {
+		matrix[i] = new BYTE[yLength]();
+	}
+
+	return matrix;
+}
+
 void FreeMatrix(REAL** matrix, int xLength) {
+	for (int i = 0; i < xLength; ++i) {
+		delete[] matrix[i];
+	}
+	delete[] matrix;
+}
+
+void FreeMatrix(BYTE** matrix, int xLength) {
 	for (int i = 0; i < xLength; ++i) {
 		delete[] matrix[i];
 	}
