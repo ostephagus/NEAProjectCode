0f4c40ae19a7cfa1bc6b493cbb63e300241129f8
Sam <37499766+ostephagus@users.noreply.github.com>
2024-01-27 18:19:21 +0000

    Added relative positioning on top of VisualisationControl

---
+++ UserInterface/Converters/RelativeDimension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UserInterface.Converters
+{
+    /// <summary>
+    /// Converts a number between 0 and 1 into a fraction of the dimension of the parent.
+    /// </summary>
+    public class RelativeDimension : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not double || parameter is not string || !double.TryParse((string)parameter, out double fractionOfParent))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+            double parentDimension = (double)value;
+            return fractionOfParent * parentDimension;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
